443c44f5a5c2f4b0c42c98c3fabb77da611d4866
Sun makoto <s2821d3721@gmail.com>
2016-01-09 23:44:05 +0800

    各頁 cover 70%

--- app/js/init-page.js
+++ app/js/init-page.js
@@ -1,6 +1,7 @@
 require('./lib/dom.js');
 
 var initPage = location.hash.slice(8) || 'home';
+location.hash = '#target-' + initPage;
 initPage = 'page-' + initPage;
 Qid('h-controller').style.height
 	= Qid(initPage).offsetHeight + 'px';
