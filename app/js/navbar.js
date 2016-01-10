8126421b011bc8bf21a7b236de9fc928c34335a7
Sun makoto <s2821d3721@gmail.com>
2016-01-10 14:22:37 +0800

    更新 navbar 樣式

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -4,6 +4,9 @@ var pages = [
 	'home', 'schedule', 'sponsor', 'location'
 ];
 
+var activePage = location.hash.slice(8) || 'home';
+addClass(Qid('link-'+activePage), 'active');
+
 for(var i=0; i<pages.length; ++i)
 	addEvent(
 		Qid('link-'+pages[i]),
@@ -13,6 +16,9 @@ for(var i=0; i<pages.length; ++i)
 			return function() {
 				Qid('h-controller').style.height
 				= Qid('page-'+which).offsetHeight + 'px';
+				removeClass(Qid('link-'+activePage), 'active');
+				activePage = which;
+				addClass(Qid('link-'+activePage), 'active');
 			};
 		}()
 	);	
