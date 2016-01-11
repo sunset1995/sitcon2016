b7ab500d6315549ecaad5b073d33de284f79348c
Sun makoto <s2821d3721@gmail.com>
2016-01-11 20:56:20 +0800

    隱藏 cover 大標初始錯位狀態

--- app/js/init-page.js
+++ app/js/init-page.js
@@ -5,3 +5,7 @@ location.hash = '#target-' + initPage;
 initPage = 'page-' + initPage;
 Qid('h-controller').style.height
 	= Qid(initPage).offsetHeight + 'px';
+
+setTimeout(function() {
+	addClass(Qid('image-group-title'), 'active');
+}, 100);
