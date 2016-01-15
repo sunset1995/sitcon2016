cda8542b7a828f1b0511ba146a847f5149c66bee
Sun makoto <s2821d3721@gmail.com>
2016-01-15 14:43:33 +0800

    新增其他活動介紹

--- app/js/main.js
+++ app/js/main.js
@@ -2,5 +2,6 @@ require('./init-page.js');
 require('./navbar.js');
 require('./footer.js');
 require('./pages/location-open.js');
+require('./pages/schedule-upground.js');
 
 console.log('Hello~ ^^');
---
+++ app/js/pages/schedule-upground.js
@@ -0,0 +1,45 @@
+require('../lib/dom.js');
+var perfectScrollBar = require('perfect-scrollbar');
+
+var eventList = [
+	'event-lighting-talk',
+	'event-salon',
+	'event-unconference',
+	'event-angel',
+	'event-code-puzzle',
+	'event-booth'
+];
+
+var fancybox = Qid('fancybox-event-detail');
+
+var closeArea = fancybox.querySelector('.closearea');
+addEvent(closeArea, 'click', function() {
+	closeFancyboxEventDetail();
+});
+
+var container = fancybox.querySelector('.container');
+perfectScrollBar.initialize(container);
+
+var content = fancybox.querySelector('.content');
+
+for(var i=0; i<eventList.length; ++i) {
+	var eventDOM = Qid(eventList[i]);
+	addEvent(eventDOM, 'click', function() {
+		var storageDOM = Qid(eventList[i]+'-info');
+		return function() {
+			content.innerHTML = storageDOM.innerHTML;
+			openFancyboxEventDetail();
+		};
+	}());
+	eventDOM = null;
+}
+
+function openFancyboxEventDetail() {
+	perfectScrollBar.update(container);
+	fancybox.style.display = 'block';
+	document.documentElement.style.overflow = 'hidden';
+}
+function closeFancyboxEventDetail() {
+	fancybox.style.display = 'none';
+	document.documentElement.style.overflow = 'auto';
+}
