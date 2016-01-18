8c07dd94743f2125b32d029a126dfbc3a637f656
Sun makoto <s2821d3721@gmail.com>
2016-01-18 13:00:55 +0800

    在 sponsor 頁面隱藏 footer sponsor

--- app/js/navbar.js
+++ app/js/navbar.js
@@ -8,13 +8,27 @@ var pages = [
 var activePage = location.hash.slice(8) || 'home';
 addClass(Qid('link-'+activePage), 'active');
 
+var footerSponsorDOM = Qid('footer-sponsor');
+if( activePage==='sponsor' )
+	footerSponsorDOM.style.display = 'none';
+
 for(var i=0; i<pages.length; ++i)
 	addEvent(
 		Qid('link-'+pages[i]),
 		'click',
 		function() {
 			var which = pages[i];
+			var footerSponsorToggle = null;
+			if( which==='sponsor' )
+				footerSponsorToggle = function() {
+					footerSponsorDOM.style.display = 'none';
+				}
+			else
+				footerSponsorToggle = function() {
+					footerSponsorDOM.style.display = 'block';
+				}
 			return function() {
+				footerSponsorToggle();
 				Qid('h-controller').style.height
 				= Qid('page-'+which).offsetHeight + 'px';
 				removeClass(Qid('link-'+activePage), 'active');
