b036af71fc0bbe904053f1011442e4ab8acb047a
Sun makoto <s2821d3721@gmail.com>
2016-01-25 23:41:10 +0800

    測試 GA 移除 html prettify

--- gulp/html.js
+++ gulp/html.js
@@ -3,15 +3,12 @@ var htmlPath = require('./_config.js').html;
 
 var gulp = require('gulp');
 var fileinclude = require('gulp-file-include');
-var prettify = require('gulp-html-prettify');
 
 gulp.task('html' , function(){
 	return gulp.src(htmlPath.src)
 		.pipe(fileinclude({
 			prefix: '__@w@'
 		}))
-		.on('error', errorLog )
-		.pipe(prettify({indent_char: '\t', indent_size: 1}))
 		.on('error', errorLog )
 		.pipe(gulp.dest(htmlPath.dst));
 });
