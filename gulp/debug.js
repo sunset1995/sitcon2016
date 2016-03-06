6a724d62d02c98a716e2060f1838012598b19655
Sun mokoto <s2821d3721@gmail.com>
2016-03-06 18:18:41 +0800

    更新：debug 時不做圖片壓縮

--- gulp/debug.js
+++ gulp/debug.js
@@ -3,7 +3,7 @@ var config = require('./_config.js');
 var gulp = require('gulp');
 var browsersSync = require('browser-sync');
 
-gulp.task('debug' , ['html', 'scss-debug', 'js-debug', 'image', 'favicon', 'json', 'cfp'] , function() {
+gulp.task('debug' , ['html', 'scss-debug', 'js-debug', 'favicon', 'json', 'cfp'] , function() {
 	var reload = browsersSync.reload;
 	browsersSync({
 		server: {
@@ -14,7 +14,6 @@ gulp.task('debug' , ['html', 'scss-debug', 'js-debug', 'image', 'favicon', 'json
 	gulp.watch(config.html.watch, ['html', reload]);
 	gulp.watch(config.js.watch, ['js-debug', reload]);
 	gulp.watch(config.scss.watch, ['scss-debug', reload]);
-	gulp.watch(config.image.watch, ['image', reload]);
 	gulp.watch(config.favicon.watch, ['favicon', reload]);
 	gulp.watch(config.favicon.watch, ['json', reload]);
 	gulp.watch(config.cfp.watch, ['cfp', reload]);
